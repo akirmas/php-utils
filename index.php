05f1bb6a5ef2c2f8a192e89b556bd773551525d2
Andrii <banderlog17@gmail.com>
2019-05-05 13:22:27 +0300

    Add functions from apis-configs.

--- index.php
+++ index.php
@@ -70,9 +70,10 @@ function formatString($format, $obj) {
 }
 
 function fillValues($obj, $voc) {
-  foreach($obj as $key => $value)
-    $obj[$key] = formatString($value, $voc);
-  return $obj;
+    $obj = (array) $obj;
+    foreach($obj as $key => $value)
+        $obj[$key] = formatString($value, $voc);
+    return $obj;
 }
 
 function fillKeys($obj, $voc) {
@@ -86,4 +87,72 @@ function fillKeys($obj, $voc) {
 function closeAndExit($code = 0) {
   session_write_close();
   exit($code);
+}
+
+function tryGet($source, $key, $defaultValue = null) {
+    return gettype($source) === 'array' && array_key_exists($key, $source)
+        ? $source[$key]
+        : (gettype($source) === 'object' && property_exists($source, $key)
+            ? $source->{$key}
+            : $defaultValue
+        );
+}
+
+function getRequestObject()
+{
+    $inputData = null;
+    $inputMethod = null;
+    if(array_key_exists('REQUEST_METHOD', $_SERVER)){
+        $requestHeaders = getallheaders();
+        switch($_SERVER['REQUEST_METHOD']){
+            case 'POST':
+                $inputMethod = 'POST';
+                //Process POSTed forms and files here:
+                if (array_key_exists('Content-Type', $requestHeaders)){
+                    $contentType = $requestHeaders['Content-Type'];
+                    switch($contentType){
+                        case 'application/x-www-form-urlencoded':
+                            $inputData = $_POST;
+                            break;
+                        case 'multipart/form-data-encoded':
+                            if(empty($_FILES)) return false;
+                            //TODO: Uploaded files processing here.
+                            break;
+                    }
+                } else {
+                    //We assume that JSON was POSTed and process it here:
+                    $inputData = json_decode(file_get_contents('php://input'));
+                    if(json_last_error() !== JSON_ERROR_NONE){
+                        return false;
+                    }
+                }
+                break;
+            case 'GET':
+                break;
+        }
+    } elseif(PHP_SAPI === 'cli') {
+        $inputMethod = 'cli';
+        $inputData = file_get_contents('php://stdin');
+    } else {
+        return false;
+    }
+    return (object)['inputMethod' => $inputMethod, 'inputData' => $inputData];
+}
+
+function getResultOfMirroredToUrlRequest($url, $request, $verifyPeerSSL = 0)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    if(is_string($request)){
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $request);
+    } elseif(is_array($request) || is_object($request)){
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($request));
+    }
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, $verifyPeerSSL);
+    $response = curl_exec($ch);
+    curl_close ($ch);
+    return $response;
 }
