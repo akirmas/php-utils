5bb5d9cde9301b0e201f45515db8707c73458b0d
Andrii <banderlog17@gmail.com>
2019-05-07 12:38:52 +0300

    Refactor a little.

--- test_func.php
+++ test_func.php
@@ -1,29 +1,19 @@
 <?php
-define("TEST_NUMBER", 2);
-
 require_once 'index.php';
 
 try {
 
-    switch(TEST_NUMBER){
-        case 1:
-            $initialJson = 'initial.json';
-            break;
-        case 2:
-            $initialJson = 'initial2.json';
-            break;
-        case 3:
-            $initialJson = 'initial3.json';
-            break;
+    for($i = 1; $i <= 4; $i ++){
+        $initialJson = "initial$i.json";
+        $initialJsonPath = 'some/path';
+        $jsonStr = file_get_contents( $initialJsonPath . '/' . $initialJson);
+        $decodedJson = json_decode($jsonStr, true);
+        if(!is_array($decodedJson)) throw new Exception('Can not decode main JSON.');
+        $refsPresentInRoot = isset($decodedJson['$ref']) ? true : false;
+        $processedJson = resolveRefs($decodedJson, $refsPresentInRoot, $initialJsonPath);
+        unset($processedJson['$ref']);
+        file_put_contents('clue_result.json', json_encode($processedJson) . "\n", FILE_APPEND);
     }
-    $initialJsonPath = 'some/path';
-    $jsonStr = file_get_contents( $initialJsonPath . '/' . $initialJson);
-    $decodedJson = json_decode($jsonStr, true);
-    if(!is_array($decodedJson)) throw new Exception('Can not decode main JSON.');
-    $refsPresentInRoot = isset($decodedJson['$ref']) ? true : false;
-    $processedJson = resolveRefs($decodedJson, $refsPresentInRoot, $initialJsonPath);
-    unset($processedJson['$ref']);
-    file_put_contents('clue_result.json', json_encode($processedJson) . "\n", FILE_APPEND);
 
 } catch(Exception $e){
     echo ' >>>>> ' . $e->getMessage() . ' <<<<< ';
